4d252da6d781f59094b7d5c6e9849743b94b3011
dquoc1602 <143944928+dquoc1602@users.noreply.github.com>
2023-11-27 09:57:42 +0700

    Update app.module.ts

--- pet-shopping-project/src/app/app.module.ts
+++ pet-shopping-project/src/app/app.module.ts
@@ -1,17 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-=======
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
->>>>>>> 484ebc408652686d0973ae8d4664f156c3802a22
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
