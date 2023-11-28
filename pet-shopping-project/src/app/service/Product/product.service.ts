5b6d31484e17b27953e32d9c8e4992869d4b51e8
minhtam900 <dieptu900@gmail.com>
2023-11-28 16:22:10 +0700

    chinh shop

--- pet-shopping-project/src/app/service/Product/product.service.ts
+++ pet-shopping-project/src/app/service/Product/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { Product } from 'src/app/model/Product/product';
 
 @Injectable({
@@ -15,4 +16,32 @@ export class ProductService {
   getAll() {
     return this.http.get<Product[]>(this._API_URL);
   }
+
+  getProduct(id:number): Observable<Product>{
+    return this.http.get<Product>('this._API_URL/${id}');
+  }
+
+  getProductByCategoryId(id: number): Observable<any> {
+    // Check if the product exists by ID
+    return this.http.get(`${this._API_URL}/${id}`).pipe(
+      ((error) => {
+        return new Observable<any>();
+      }),
+      ((response: any) => {
+        // Assuming your backend returns a valid product
+        const category = response.category;
+
+        
+        return this.http.get(`${this._API_URL}/category/${category}`).pipe(
+          ((error) => {
+            // Handle error, for example, return a not-found response
+            return new Observable<any>(); // Replace with proper error handling
+          })
+        );
+      })
+    );
+  }
+  // getByCategory(id: number): Observable<Product[]> {
+  //   return this.http.get<Product[]>(this._API_URL + '/' + id);
+  // }
 }
